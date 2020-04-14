71aec93a07db04767b536478e38da61502cf38b4
hernandez87v <hernandez87v@hotmail.com>
2020-04-14 09:59:13 -0700

    Started writing DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import DayListItem from 'components/DayListItem';
+
+const day = days[0];
 
 function DayList(props) {
-  return <ul></ul>;
+  const days = props.days.map((day) => {
+    return (
+      //   <ul>
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+      //   </ul>
+    );
+  });
+  return days;
 }
+
+ReactDOM.render(
+  <DayList days={days} day={day} setDay={action({ setDay })} />,
+  document.getElementById('root')
+);
