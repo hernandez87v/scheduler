bc3fba6abd1ef0314c4d4d1a162501684e5eda04
hernandez87v <hernandez87v@hotmail.com>
2020-04-15 09:27:48 -0700

    Fixed Header Component/Story

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Appointment(props) {
+  <main className="appointment__add">
+    <img className="appointment__add-button" src="images/add.png" alt="Add" />
+  </main>;
+}
