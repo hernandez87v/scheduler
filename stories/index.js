65879028189ff1c856fa6450e4ce57022ba8a9e4
hernandez87v <hernandez87v@hotmail.com>
2020-04-15 14:58:18 -0700

    Added Error Component/Story

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from 'components/Appointment/Empty.jsx';
 import Show from 'components/Appointment/Show.jsx';
 import Confirm from 'components/Appointment/Confirm.jsx';
 import Status from 'components/Appointment/Status.jsx';
+import Error from 'components/Appointment/Error.jsx';
 
 storiesOf('Button', module)
   .addParameters({
@@ -155,4 +156,10 @@ storiesOf('Appointment', module)
       onCancel={action('onCancel')}
     />
   ))
-  .add('Status', () => <Status message={'Deleting'} />);
+  .add('Status', () => <Status message={'Deleting'} />)
+  .add('Error', () => (
+    <Error
+      message={'Could not delete appointment.'}
+      onClose={action('onClose')}
+    />
+  ));
