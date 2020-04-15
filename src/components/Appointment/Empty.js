f2e2a3967d29d0c5fd63407f871f6c4e8312ae8c
hernandez87v <hernandez87v@hotmail.com>
2020-04-15 09:38:29 -0700

    Added Empty Component/Story

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 export default function Appointment(props) {
-  <main className="appointment__add">
-    <img className="appointment__add-button" src="images/add.png" alt="Add" />
-  </main>;
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
+    </main>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
 
 storiesOf('Button', module)
   .addParameters({
@@ -134,4 +135,5 @@ storiesOf('Appointment', module)
     backgrounds: [{ name: 'white', value: '#fff', default: true }],
   })
   .add('Appointment', () => <Appointment />)
-  .add('Header', () => <Header time="12pm" />);
+  .add('Header', () => <Header time="12pm" />)
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
