4f8e797891b88f50095608e59bd51f83d5f43bca
hernandez87v <hernandez87v@hotmail.com>
2020-04-15 19:36:22 -0700

    Added Appointment Empty Component/Story

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import Header from 'components/Appointment/Header.jsx';
+import Show from 'components/Appointment/Show.jsx';
+import Empty from 'components/Appointment/Empty.jsx';
 import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  const { time, interview } = props;
+  return (
+    <>
+      <Header time={time} />
+      <article time={time} className="appointment">
+        {interview ? (
+          <Show
+            student={interview.student}
+            interviewer={interview.interviewer.name}
+          />
+        ) : (
+          <Empty />
+        )}
+      </article>
+    </>
+  );
 }
