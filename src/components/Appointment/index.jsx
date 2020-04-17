b24014d7be6d67eb9494b0a4cec6f60a141f7b2a
hernandez87v <hernandez87v@hotmail.com>
2020-04-16 19:50:24 -0700

    Changed PORT for axios and added import to index

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -2,21 +2,39 @@ import React from 'react';
 import Header from 'components/Appointment/Header.jsx';
 import Show from 'components/Appointment/Show.jsx';
 import Empty from 'components/Appointment/Empty.jsx';
+import Form from 'components/Appointment/Form';
 import 'components/Appointment/styles.scss';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
-  const { time, interview } = props;
+  const { time, interview, onEdit, onDelete, onSave, interviewers } = props;
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <>
       <Header time={time} />
       <article time={time} className="appointment">
-        {interview ? (
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === CREATE && (
+          <Form
+            name={''}
+            interviewers={interviewers}
+            interviewer={[]}
+            onSave={onSave}
+            onCancel={() => back()}
+          />
+        )}
+        {mode === SHOW && (
           <Show
             student={interview.student}
             interviewer={interview.interviewer.name}
+            onEdit={onEdit}
+            onDelete={onDelete}
           />
-        ) : (
-          <Empty />
         )}
       </article>
     </>
