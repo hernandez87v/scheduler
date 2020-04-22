b809ba4abb97e1c0bd68dd5ef0fa611668baaffa
hernandez87v <hernandez87v@hotmail.com>
2020-04-22 14:53:22 -0700

    Added tests for Appointments to create new appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,6 +2,14 @@ describe('Appointments', () => {
   it('should book an interview', () => {
     cy.visit('/');
 
-    cy.contains('[data-testid=day]', 'Monday');
+    cy.contains('Monday');
+
+    cy.get('[alt=Add]').first().click();
+
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains('Save').click();
   });
 });
