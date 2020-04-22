f0418bdfa6658ed2592326c4df1c710e45f319fa
hernandez87v <hernandez87v@hotmail.com>
2020-04-22 15:22:03 -0700

    Refactored test to group beforeEach seperately

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,10 +2,9 @@ describe('Appointments', () => {
   beforeEach(() => {
     cy.request('GET', 'http://localhost:3000/api/debug/reset');
     cy.visit('/');
+    cy.contains('Monday');
   });
   it('should book an interview', () => {
-    cy.contains('Monday');
-
     cy.get('[alt=Add]').first().click();
 
     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
@@ -13,5 +12,8 @@ describe('Appointments', () => {
     cy.get("[alt='Sylvia Palmer']").click();
 
     cy.contains('Save').click();
+
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
 });
